59ced41abbadf748f5a4f0a98f4fe1465d9d4394
CristiVolo <cristianvolosciuc@yahoo.com>
2022-06-11 15:29:09 +0300

    Added units of measurement on graph, and conditional rendering of the 2 datasets for each abscissa/ordinate.

--- components/LineGraph.js
+++ components/LineGraph.js
@@ -2,29 +2,26 @@ import React from "react";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import { Dimensions } from "react-native";
 import { LineChart } from "react-native-chart-kit";
+import SwitchSelector from "react-native-switch-selector";
 
 const LineGraph = ({
+    onPress,
     backgroundGradientFrom,
     backgroundGradientTo,
     color,
     strokeWidth,
     legend,
     abscissaData,
-    ordinateData,
+    ordinateData
 }) => {
-    const data = {
+    let data = {
         labels: abscissaData,
         datasets: [
             {
                 data: ordinateData,
                 color: () => color, //`rgba(0, 0, 255, ${opacity})`,
                 strokeWidth: strokeWidth, // optional
-            },
-            {
-                data: ordinateData,
-                color: () => color, //`rgba(0, 0, 255, ${opacity})`,
-                strokeWidth: strokeWidth, // optional
-            },
+            }
         ],
         legend: legend, // optional
     };
@@ -32,8 +29,8 @@ const LineGraph = ({
     const chartConfig = {
         backgroundGradientFrom: backgroundGradientFrom,
         backgroundGradientTo: backgroundGradientTo,
-        fillShadowGradientFrom: backgroundGradientFrom,
-        fillShadowGradientTo: backgroundGradientTo,
+        // fillShadowGradientFrom: backgroundGradientFrom,
+        // fillShadowGradientTo: backgroundGradientTo,
         decimalPlaces: 2, // optional, defaults to 2dp
         color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
         labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
@@ -49,6 +46,31 @@ const LineGraph = ({
 
     return (
         <View>
+            <SwitchSelector
+                initial={0}
+                style={{
+                    justifyContent: "center",
+                    alignSelf: "center",
+                    width: 300,
+                    marginTop: 50,
+                    position: "relative",
+                }}
+                onPress={onPress}
+                options={[
+                    {
+                        // Most recent week
+                        label: "24-Hour span",
+                        value: false,
+                        activeColor: "deepskyblue",
+                    },
+                    {
+                        // Most recent day
+                        label: "7-Day span",
+                        value: true,
+                        activeColor: "dodgerblue",
+                    },
+                ]}
+            />
             <LineChart
                 data={data}
                 width={screenWidth}
--- screens/GraphScreen.js
+++ screens/GraphScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import TransferButton from "../components/TransferButton";
 import { SafeAreaView } from "react-native";
@@ -6,6 +6,50 @@ import LineGraph from "../components/LineGraph";
 import axios from "axios";
 
 const GraphScreen = ({ navigation }) => {
+
+    let parsedReadDate = "";
+    let newParsedReadDate = "";
+    let parsedMonth = "";
+    let parsedDay = "";
+
+    const month = {
+        "1": "Jan",
+        "2": "Feb",
+        "3": "Mar",
+        "4": "Apr",
+        "5": "May",
+        "6": "Jun",
+        "7": "Jul",
+        "8": "Aug",
+        "9": "Sep",
+        "10": "Oct",
+        "11": "Nov",
+        "12": "Dec",
+    };
+
+    const [airTemperatureDataSwitch, setAirTemperatureDataSwitch] = useState(false);
+    const [airHumidityDataSwitch, setAirHumidityDataSwitch] = useState(false);
+    const [lightDataSwitch, setLightDataSwitch] = useState(false);
+    const [gasConcentrationDataSwitch, setGasConcentrationDataSwitch] = useState(false);
+    const [waterLevelDataSwitch, setWaterLevelDataSwitch] = useState(false);
+    const [soilMoistureDataSwitch, setSoilMoistureDataSwitch] = useState(false);
+
+    const [airTemperatureLastDay, setAirTemperatureLastDay] = useState([20, 45]);
+    const [airHumidityLastDay, setAirHumidityLastDay] = useState([20, 45]);
+    const [lightLastDay, setLightLastDay] = useState([20, 45]);
+    const [gasConcentrationLastDay, setGasConcentrationLastDay] = useState([20, 45]);
+    const [waterLevelLastDay, setWaterLevelLastDay] = useState([20, 45]);
+    const [soilMoistureLastDay, setSoilMoistureLastDay] = useState([20, 45]);
+    const [readDateLastDay, setReadDateLastDay] = useState(["January", "February"]);
+
+    const [airTemperatureLastWeek, setAirTemperatureLastWeek] = useState([11, 22]);
+    const [airHumidityLastWeek, setAirHumidityLastWeek] = useState([11, 22]);
+    const [lightLastWeek, setLightLastWeek] = useState([11, 22]);
+    const [gasConcentrationLastWeek, setGasConcentrationLastWeek] = useState([11, 22]);
+    const [waterLevelLastWeek, setWaterLevelLastWeek] = useState([11, 22]);
+    const [soilMoistureLastWeek, setSoilMoistureLastWeek] = useState([11, 22]);
+    const [readDateLastWeek, setReadDateLastWeek] = useState(["July", "August"]);
+
     // D:\xampp\htdocs\greenlab\sensor_data.php
     const wiFiIPv4 = "192.168.0.101";
     const htdocsFolder = "greenlab";
@@ -20,11 +64,48 @@ const GraphScreen = ({ navigation }) => {
                 .then((response) => {
                     console.log("-------------------------------------------");
                     console.log("RESPONSE TO GET");
-                    console.log("-------------------------------------------");
                     console.log(response.data);
+                    console.log("-------------------------------------------");
                     console.log(
                         "~-------------------------------------------~"
                     );
+
+                    // Last day
+                    // for (let i = 0; i < response.data.last_day.length; i++) {
+                    //     airTemperatureLastDay[i] = response.data.last_day[i].air_temperature;
+                    //     airHumidityLastDay[i] = response.data.last_day[i].air_humidity;
+                    //     lightLastDay[i] = response.data.last_day[i].light;
+                    //     gasConcentrationLastDay[i] = response.data.last_day[i].gas_concentration;
+                    //     waterLevelLastDay[i] = response.data.last_day[i].water_level;
+                    //     soilMoistureLastDay[i] = response.data.last_day[i].soil_moisture;
+
+                    //     parsedReadDate = response.data.last_day[i].read_date;
+                    //     parsedDay = parsedReadDate.slice(9, 10);
+                    //     parsedHour = parsedReadDate.slice(11, 13);
+                    //     newParsedReadDate = parsedDay + 'h' + parsedHour;
+                    //     readDateLastDay[i] = newParsedReadDate;
+                    //     console.log(airTemperatureLastDay[i],' ',airHumidityLastDay[i],' ',airHumidityLastDay[i]);
+                    // }
+
+                    // // Last week
+                    // for (let i = 0; i < response.data.last_week.length; i++) {
+                    //     airTemperatureLastWeek[i] = response.data.last_week[i].avg_air_temperature;
+                    //     airHumidityLastWeek[i] = response.data.last_week[i].avg_air_humidity;
+                    //     lightLastWeek[i] = response.data.last_week[i].avg_light;
+                    //     gasConcentrationLastWeek[i] = response.data.last_week[i].avg_gas_concentration;
+                    //     waterLevelLastWeek[i] = response.data.last_week[i].avg_water_level;
+                    //     soilMoistureLastWeek[i] = response.data.last_week[i].avg_soil_moisture;
+
+                    //     parsedReadDate = response.data.last_week[i].read_date;
+                    //     parsedMonth = month[response.data.last_week[i].read_month];
+                    //     parsedDay = response.data.last_week[i].read_day;
+                    //     newParsedReadDate = parsedMonth + ', ' + parsedDay;
+                    //     readDateLastWeek[i] = newParsedReadDate;
+                        
+                    //     console.log(airTemperatureLastWeek[i],' ',airHumidityLastWeek[i],' ',lightLastWeek[i]);
+                    // }
+
+                    
                 })
                 .catch((err) => {
                     console.log(err);
@@ -45,100 +126,78 @@ const GraphScreen = ({ navigation }) => {
                     color="lightseagreen"
                 />
                 <LineGraph
+                    onPress={() => {
+                        setAirTemperatureDataSwitch(!airTemperatureDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="steelblue"
                     strokeWidth={4}
-                    legend={["Air Temperature"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[airTemperatureDataSwitch ? "Air Temperature(day/°C)" : "Air Temperature(hour/°C)" ]}
+                    abscissaData={airTemperatureDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={airTemperatureDataSwitch ? airTemperatureLastWeek : airTemperatureLastDay}
                 />
                 <LineGraph
+                    onPress={() => {
+                        setAirHumidityDataSwitch(!airHumidityDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="teal"
                     strokeWidth={4}
-                    legend={["Air Humidity"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[airHumidityDataSwitch ? "Air Humidity(day/%)" : "Air Humidity(hour/%)"]}
+                    abscissaDataDay={readDateLastDay}
+                    ordinateDataDay={airHumidityLastDay}
+                    abscissaData={airHumidityDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={airHumidityDataSwitch ? airHumidityLastWeek : airHumidityLastDay}
                 />
                 <LineGraph
+                    onPress={() => {
+                        setLightDataSwitch(!lightDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="orange"
                     strokeWidth={4}
-                    legend={["Light"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[lightDataSwitch ? "Light(day/(0 - light, 1 - dark))" : "Light(hour/(0 - light, 1 - dark))"]}
+                    abscissaData={lightDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={lightDataSwitch ? lightLastWeek : lightLastDay}
                 />
                 <LineGraph
+                    onPress={() => {
+                        setGasConcentrationDataSwitch(!gasConcentrationDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="rebeccapurple"
                     strokeWidth={4}
-                    legend={["Gas Concentration"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[gasConcentrationDataSwitch ? "Gas Concentration(day/ppm)" : "Gas Concentration(hour/ppm)"]}
+                    abscissaData={gasConcentrationDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={gasConcentrationDataSwitch ? gasConcentrationLastWeek : gasConcentrationLastDay}
                 />
                 <LineGraph
+                    onPress={() => {
+                        setWaterLevelDataSwitch(!waterLevelDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="skyblue"
                     strokeWidth={4}
-                    legend={["Water Level"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[waterLevelDataSwitch ? "Water Level(day/%)" : "Water Level(hour/%)"]}
+                    abscissaData={waterLevelDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={waterLevelDataSwitch ? waterLevelLastWeek : waterLevelLastDay}
                 />
                 <LineGraph
+                    onPress={() => {
+                        setSoilMoistureDataSwitch(!soilMoistureDataSwitch);
+                    }}
                     backgroundGradientFrom="ivory"
                     backgroundGradientTo="lavender"
                     color="tan"
                     strokeWidth={4}
-                    legend={["Soil Moisture"]}
-                    abscissaData={[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                    ]}
-                    ordinateData={[20, 45, 28, 80, 99, 43]}
+                    legend={[soilMoistureDataSwitch ? "Soil Moisture(day/%)" : "Soil Moisture(hour/%)"]}
+                    abscissaData={soilMoistureDataSwitch ? readDateLastWeek : readDateLastDay}
+                    ordinateData={soilMoistureDataSwitch ? soilMoistureLastWeek : soilMoistureLastDay}
                 />
             </SafeAreaView>
         </ScrollView>
